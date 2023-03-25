c2ce9a8f32345c86cd13335d3b075ca80cd8dbd9
Ribinsh <ribinshp7@gmail.com>
2023-03-25 14:41:50 +0530

    review completed

---
+++ practice/MergeSort/sample.js
@@ -0,0 +1,35 @@
+function mergeSort(arr){
+    if(arr.length<= 1) return arr
+
+    let mid = Math.floor((arr.length)/2)
+    let left = mergeSort(arr.slice(0,mid))
+    let right = mergeSort(arr.slice(mid))
+
+    return merge(left, right)
+}
+
+function merge(arr1 ,arr2){
+    let results = [];
+    let i =0;
+    let j = 0 ;
+    while(i< arr1.length && j< arr2.length){
+        if(arr1[i] <= arr2[j]){
+            results.push(arr1[i])
+            i++;
+        }else{
+            results.push(arr2[j])
+            j++;
+        }
+    }
+    while( i< arr1.length){
+        results.push(arr1[i])
+        i++
+    }
+    while (j < arr2.length){
+        results.push(arr2[j])
+        j++;
+    }
+    return results
+}
+
+console.log(mergeSort([4,6,3,7,1,9,2]));
